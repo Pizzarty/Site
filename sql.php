f6b86b1dbce8f0008f1073d0f0bf4e0c8f3ba610
Pizzarty <pierre.banville@laposte.net>
2016-02-25 18:08:11 +0100

    Update sql.php

--- sql.php
+++ sql.php
@@ -4,10 +4,10 @@ $user=$_POST['pseudo'];
 $mdp=$_POST['password'];
 $sql=mysql_query("SELECT prenom, nom, mail FROM UserConnexion WHERE identifiant=$user AND password=$mdp");
 	if(mysql_num_rows($sql)==0)
-	}
+	{
 	echo "Echec d'authentification. Identifiant ou mot de passe incorrecte.";
 	else
-	{
+	}
 	$ligne=mysql_fetch_array($sql); 
 	echo "Bonjour $ligne[prenom] $ligne[nom]";
 	$sql2=mysql_query("SELECT nom, prenom, mail FROM UserConnexion");
@@ -17,6 +17,5 @@ $sql=mysql_query("SELECT prenom, nom, mail FROM UserConnexion WHERE identifiant=
 	{
 	echo $ligne2[nom]." ".$ligne2[prenom]." ".$ligne2[mail]."<br/>";
 	$ligne2=mysql_fetch_array($sql2); 
-	}
 	}
 ?>
